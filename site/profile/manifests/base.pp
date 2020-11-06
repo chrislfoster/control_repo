65ae5afb45e7d2b8d43c5a38b16af6db61fb759e
cfoster <51326213+chrislfoster@users.noreply.github.com>
2020-11-06 10:05:45 -0600

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,11 +1,5 @@
 class profile::base {
   user {'admin':
-  ensure => present,
- }
-}
-
-class { 'docker':
-  docker_ee => true,
-  docker_ee_source_location => 'https://<docker_ee_repo_url>',
-  docker_ee_key_source => 'https://<docker_ee_key_source_url>',
+    ensure => present
+  }
 }
