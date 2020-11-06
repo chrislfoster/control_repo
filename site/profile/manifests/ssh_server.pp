b3dc5fa61e2c9b285b15c584f20934fe67137ffc
cfoster <51326213+chrislfoster@users.noreply.github.com>
2020-11-06 14:39:21 -0600

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => '',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDQzSIMmo5pX96LhAlokaLWxeIcx1hHucQHSnhxsBGvfgKNYIyUxHViR2dwJTSVx53W7PMVHV3RZD+VnWXqdyQ++xKFxPlkal59sstFYKTr0Hy5cdog11hSPmVISlXr/5xamuXXNLfo47p4cfkSk93EQxcg+FqJK6B22lUEqPoxtraCDYTbvlw9wtzROz7YGOt1DV2FaeMW8dA/mwmKkwKF/ZqYDuDEBy/7bpaGVdfVxg/Vdm+QH+eSnY0WMp+OqaBajB4er1ahNmGhVM+mpfldqpWhzlWTZGOPWgNJj2a7loEfPBaHcWll+pmCEbU/X+uNa1Q5StYz8TNXaJANVHTT',
   }  
 }
