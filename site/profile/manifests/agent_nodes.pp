cfd2d6154b38ecd49f588ad5e7f67f8f5569ab4b
cfoster <51326213+chrislfoster@users.noreply.github.com>
2020-11-06 10:03:41 -0600

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,9 +1,5 @@
 class profile::agent_nodes {
-  include 'docker'
-  include dockeragent::node {'web.puppet.vm':}
-  include dockeragent::node {'db.puppet.vm':}
-}   
-
-class { 'docker':
-  use_upstream_package_source => false,
+  include dockeragent
+  dockeragent::node { 'web.puppet.vm': }
+  dockeragent::node { 'db.puppet.vm': }
 }
